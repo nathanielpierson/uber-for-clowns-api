73cb87b1aa118a57a2e972828746e91f06c9ee64
alejandrojuarez117 <aljuarez117@gmail.com>
2025-03-27 20:22:36 -0600

    added email and password_digest to clowns so clowns can make accounts and edited jbuilder/controller

--- app/views/clowns/_clown.json.jbuilder
+++ app/views/clowns/_clown.json.jbuilder
@@ -1,5 +1,7 @@
-json.id clown.id
-json.name clown.name
-json.specialty clown.specialty
-json.rates clown.rates
-json.created_at clown.created_at
+json.id               clown.id
+json.email            clown.email
+json.password_digest  clown.password_digest
+json.name             clown.name
+json.specialty        clown.specialty
+json.rates            clown.rates
+json.created_at       clown.created_at
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_21_002543) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_28_021009) do
   create_table "bookings", force: :cascade do |t|
     t.integer "clown_id"
     t.integer "user_id"
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_21_002543) do
     t.integer "rates"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
   create_table "users", force: :cascade do |t|
